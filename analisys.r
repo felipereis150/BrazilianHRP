f08754da2e3b48ce2ead0d07169f4ad91a6b4d96
Felipe <felipereis150@gmail.com>
2022-08-07 15:10:48 -0300

    initial test

--- analisys.r
+++ analisys.r
@@ -1,1 +1,54 @@
-print("hello world")
+# importing packages
+install.packages("HierPortfolios")
+install.packages("readxl")
+install.packages("httr")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("lubridate")
+
+library("HierPortfolios")
+library("readxl")
+library("httr")
+library("dplyr")
+library("tidyr")
+library("stringr")
+library("lubridate")
+
+# reading data file
+df <- read_xlsx("ibrx_mensal.xlsx", na = "-")
+
+# creating a dataframe with only the necessary columns and renaming the columns
+colnames(df) <- str_replace(colnames(df), "Retorno\r\ndo fechamento\r\nem 1 mês\r\nEm moeda orig\r\najust p/ prov\r\n", "")
+colnames(df)
+
+
+# trasnforming the 'Data' column to a date format
+df <- df %>%
+  mutate(Data = str_replace(Data, "Jan", "01"), 
+         Data = str_replace(Data, "Fev", "02"),
+         Data = str_replace(Data, "Mar", "03"), 
+         Data = str_replace(Data, "Abr", "04"),
+         Data = str_replace(Data, "Mai", "05"), 
+         Data = str_replace(Data, "Jun", "06"),
+         Data = str_replace(Data, "Jul", "07"), 
+         Data = str_replace(Data, "Ago", "08"),
+         Data = str_replace(Data, "Set", "09"), 
+         Data = str_replace(Data, "Out", "10"),
+         Data = str_replace(Data, "Nov", "11"), 
+         Data = str_replace(Data, "Dez", "12")) %>% 
+  mutate(Data = lubridate::my(Data)) %>% 
+  dplyr::filter(Data >= '2000-01-01', Data <= '2022-06-01')
+
+glimpse(df)
+
+# creating csv file with 4 assests with >20 years of data to test clustering
+write.csv(df[, c("IBOV", "USIM5", "CSNA3", "SBSP3")], file= "assets.csv", row.names=FALSE, sep=",")
+
+df2 <- read.csv("assets.csv", na = "-")
+glimpse(df2)
+
+# plotting the data
+wi <- HRP_Portfolio(cov(df2), graph = TRUE)
+
+# talvez valha fazer um estudo da alocação entre empresas, analisando o retorno mensal de cada empresa em carteiras de investimento públicas e privadas
