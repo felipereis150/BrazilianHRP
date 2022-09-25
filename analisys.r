1b1e5f1c31dfb8644cbbe9cdab112f746fe73d4c
Felipe <felipereis150@gmail.com>
2022-09-25 20:46:45 -0300

    style: before review

--- analisys.r
+++ analisys.r
@@ -19,10 +19,15 @@ library("lubridate")
 df <- read_xlsx("C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\ibrx_mensal.xlsx", na = "-")
 path_out <- "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\"
 
+# function to save files quickly
+save_file <- function(file_name, file_path, file_content) {
+  file_path <- paste0(file_path, file_name)
+  return(write.csv(file_content, file_path, row.names = FALSE))
+}
+
 # creating a dataframe with only the necessary columns and renaming the columns
 colnames(df) <- str_replace(colnames(df), "Retorno\r\ndo fechamento\r\nem 1 mês\r\nEm moeda orig\r\najust p/ prov\r\n", "")
 colnames(df)
-
 
 # trasnforming the 'Data' column to a date format
 df <- df %>%
@@ -44,23 +49,25 @@ df <- df %>%
 # replacing NA values with 0
 df <- df %>%
   select_if(~ !any(is.na(.)))
-glimpse(df) 
 
 i = 1
-janela_estimacao <- 60  ## (60 meses = 5 anos)
-periodo_fora_da_amostra <- (nrow(df) - janela_estimacao) - 1
-pesos = matrix(NA, ncol = periodo_fora_da_amostra, nrow = ncol(df) - 1) # -1 é para não contar a coluna que tem as datas
+periodo_de_amostra <- df %>% 
+  dplyr::filter(Data >= '2017-06-01', Data <= '2022-06-01') %>% # 5 years window
+  select(-Data) %>%  # removing the date column
+  as.matrix()  # transforming the dataframe to a matrix
 
-for (i in 1:periodo_fora_da_amostra) {
-  janela <- df[i:(i + janela_estimacao - 1), -1]
-  pesos[, i] = HRP_Portfolio(cov(janela), graph = FALSE)  # salvar como matriz de dados
-  retornos_da_carteira_one_step_ahead = pesos[, i]*df[i + janela_estimacao - 1 + 1, -1]  #salvo a carteira realizada, se der erro, tente com t(pesos[,i])
+for (i in 1:periodo_de_amostra) {
+  periodo_de_amostra[i] = HRP_Portfolio(cov(periodo_de_amostra), graph = FALSE)
 }
+#row.names(periodo_de_amostra[i]) <- colnames(periodo_de_amostra)
+save_file("periodo_de_amostra.csv", path_out, periodo_de_amostra)
 
-# Salvar em cada iteração demora muito, melhor salvar apenas no final
-row.names(pesos) = colnames(df[,-1])  # pronto, já tem os nomes dos ativos na matriz de pesos
 
-write.csv(pesos, file.path(path_out, "pesos.csv"), row.names=FALSE)
-write.csv(df, file.path(path_out, "df.csv"), row.names=FALSE)
-write.csv(retornos_da_carteira_one_step_ahead, file.path(path_out, "retornos_da_carteira_one_step_ahead.csv"), row.names=FALSE)
+# for (i in 1:periodo_de_amostra) {
+#   pesos[i] = HRP_Portfolio(cov(periodo_de_amostra), graph = FALSE)
+#   retornos_da_carteira_one_step_ahead = pesos[i]*df[i + pesos - 1 + 1, -1]
+# }
 
+# save_file("pesos.csv", path_out, pesos)
+# save_file("df.csv", path_out, df)
+# save_file("retornos_da_carteira_one_step_ahead.csv", path_out, retornos_da_carteira_one_step_ahead)
