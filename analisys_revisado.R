f2dd7742bbb36809ba6afac3e640c194ce8be806
Felipe <felipereis150@gmail.com>
2022-09-26 22:43:33 -0300

    style: reorg code

--- analisys_revisado.R
+++ analisys_revisado.R
@@ -18,10 +18,6 @@ path_out = "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\"
 
 df <- read_xlsx("C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\ibrx_mensal.xlsx", na = "-")
 colnames(df) <- str_replace(colnames(df), "Retorno\r\ndo fechamento\r\nem 1 mês\r\nEm moeda orig\r\najust p/ prov\r\n", "")
-colnames(df)
-str(df)
-
-glimpse(df)
 
 # transforming the 'Data' column to a date format
 df <- df %>%
@@ -47,59 +43,7 @@ df <- df %>%
 periodo_de_amostra = nrow(df)
 pesos_da_amostra = matrix(NA,nrow = nrow(df)-1, ncol = ncol(df)-1) #tirando a primeira coluna
 
-# 6 months rolling window
-for (i in 1:60) {
-    df_rolling = df[i:(i+60-1), 2:ncol(df)]
-    df_cov = cov(df_rolling)
-    #print (cov(df_cov)) #checando...
-    pesos_da_amostra[i,] =  t(HRP_Portfolio(df_cov, graph = FALSE))
-    glimpse(df_rolling)
-}
-
-  row.names(pesos_da_amostra) <- colnames(periodo_de_amostra)
-  save_file("periodo_de_amostra.csv", path_out, periodo_de_amostra)
-  
-  
-save_file("df_rolling.csv", path_out, df_rolling)
-save_file("df.csv", path_out, df)
-save_file("pesos_da_amostra.csv", path_out, pesos_da_amostra)
-
-# 2 years rolling window
-
-for (i in 1:60) {
-    df_rolling = df[i:(i+60-1), 2:ncol(df)]
-    df_cov = cov(df_rolling)
-    #print (cov(df_cov)) #checando...
-    pesos_da_amostra[i,] =  t(HRP_Portfolio(df_cov, graph = FALSE))
-}
-
-  row.names(pesos_da_amostra) <- colnames(periodo_de_amostra)
-  save_file("periodo_de_amostra.csv", path_out, periodo_de_amostra)
-  
-  
-save_file("df_rolling.csv", path_out, df_rolling)
-save_file("df.csv", path_out, df)
-save_file("pesos_da_amostra.csv", path_out, pesos_da_amostra)
-
 # 5 years rolling window
-
-for (i in 1:60) {
-    df_rolling = df[i:(i+60-1), 2:ncol(df)]
-    df_cov = cov(df_rolling)
-    #print (cov(df_cov)) #checando...
-    pesos_da_amostra[i,] =  t(HRP_Portfolio(df_cov, graph = FALSE))
-}
-
-  row.names(pesos_da_amostra) <- colnames(periodo_de_amostra)
-  save_file("periodo_de_amostra.csv", path_out, periodo_de_amostra)
-  
-  
-save_file("df_rolling.csv", path_out, df_rolling)
-save_file("df.csv", path_out, df)
-save_file("pesos_da_amostra.csv", path_out, pesos_da_amostra)
-
-# 10 years rolling window
-
 for (i in 1:60) {
     df_rolling = df[i:(i+60-1), 2:ncol(df)]
     df_cov = cov(df_rolling)
