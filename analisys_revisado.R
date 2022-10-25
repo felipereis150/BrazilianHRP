c7e9816a02f33f283314874f934ec69a5e4535bd
Carlos Trucios Maza <ctrucios@gmail.com>
2022-10-25 11:57:42 -0300

    Add files via upload
    Fixed Data
    Add Minimum Variance Portfolio
    Add plot

--- analisys_revisado.R
+++ analisys_revisado.R
@@ -1,11 +1,12 @@
-library("HierPortfolios")
-library("readxl")
-library("httr")
-library("dplyr")
-library("tidyr")
-library("stringr")
-library("lubridate")
-library("readr")
+library(HierPortfolios)
+library(readxl)
+library(httr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(readr)
+library(RiskPortfolios)
 
 # function to save files quickly
 save_file <- function(file_name, file_path, file_content) {
@@ -13,10 +14,10 @@ save_file <- function(file_name, file_path, file_content) {
   return(write.csv(file_content, file_path, row.names = FALSE))
 }
 
-path = "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\ibrx_mensal.xlsx"
-path_out = "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\"
+#path = "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\ibrx_mensal.xlsx"
+#path_out = "C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\"
 
-df <- read_xlsx("C:\\Users\\felip\\OneDrive\\Área de Trabalho\\BDAQ\\data\\ibrx_mensal.xlsx", na = "-")
+df <- read_xlsx("/Users/ctruciosm/Downloads/bdaq_portfolio_allocation-main/data/ibrx_mensal.xlsx", na = "-")
 colnames(df) <- str_replace(colnames(df), "Retorno\r\ndo fechamento\r\nem 1 mês\r\nEm moeda orig\r\najust p/ prov\r\n", "")
 
 # transforming the 'Data' column to a date format
@@ -37,36 +38,36 @@ df <- df %>%
   dplyr::filter(Data >= '2000-01-01', Data <= '2022-06-01')
 
 # replacing NA values with 0
-df <- df %>%
-  select_if(~ !any(is.na(.)))
+df <- df %>% select_if(~ !any(is.na(.))) # 27 - 1 assets
 
 
 n = nrow(df)
-p = ncol(df) - 1
-#w = matrix(NA, 1000, 1000) 
-w = matrix(NA, 300, 300)
-#w = matrix(NA, nrow(df), ncol(df))
+p = ncol(df)  # cotamos quantas colunas temos
+w = matrix(NA, p - 1, p - 1) # p - 1 pois a primeira coluna são apenas datas
+w_mv = matrix(NA, p - 1, p - 1) # p - 1 pois a primeira coluna são apenas datas
 
 # Defino tamanho da janela (window size) e o tamanho do periodo fora da amostra (Out-of-Sample)
-WS = 60
+WS = 60  # Utiliamos 5 anos para estimar os pesos das carteiras
 OoS = n - WS
-Rport = matrix(NA, ncol = 2, nrow = OoS) 
+Rport = matrix(NA, ncol = 3, nrow = OoS)  # 3 para incluir variância minima
 
 # 5 years rolling window
 for (i in 1:OoS) {
-    df_rolling = df[i:(i+WS-1), 2:p]
+    df_rolling = df[i:(i + WS - 1), 2:p]
     df_cov = cov(df_rolling)
     w[i,] =  t(HRP_Portfolio(df_cov, graph = FALSE))
-    Rport[i, 1] = mean(df[WS+i, 2:p])          # Carteria de pesos iguais realizada # ESTÁ RETORNANDO NA, CONSERTAR
-    Rport[i, 2] = sum(w[i, ] * df[WS+i, 2:p])  # Carteira realizada (utilizando o HRP)
+    w_mv[i,] =  optimalPortfolio(Sigma = df_cov, control = list(type = 'minvol', constraint = 'lo'))
+    Rport[i, 1] = mean(as.numeric(df[WS + i, 2:p]))   # Carteria realizada de pesos iguais 
+    Rport[i, 2] = sum(w[i, ] * df[WS + i, 2:p])       # Carteira realizada (utilizando o HRP)
+    Rport[i, 3] = sum(w_mv[i, ] * df[WS + i, 2:p])       # Carteira realizada (utilizando o HRP)
 }
 
-colnames(Rport) <- c("Equal Weights", "HRP") # nome das colunas 
-Rport <- cbind(df[WS:269, 1], Rport) # adicionando coluna dedata
+colnames(Rport) <- c("Equal Weights", "HRP", "MV") # nome das colunas 
+Rport <- cbind(df[-c(1:WS), 1], Rport)             # Tinha um erro no codigo
 
-Rport
+ts.plot(apply(Rport, 2, cumsum), lty = 1:3, col = c("red", "blue", "black"))
 
-# save_file("pesos.csv", path_out, w)
-# save_file("Rport.csv", path_out, Rport)
+#save_file("pesos_HRP.csv", path_out, w)
+#save_file("pesos_MV.csv", path_out, w_mv)
+#save_file("Rport.csv", path_out, Rport)
 
-# IMPLEMENTAR OUTROS MÉTODOS PARA COMPARAÇÃO
